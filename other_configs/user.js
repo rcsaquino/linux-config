f19802cb3d33311444d952749ed48f701a932829
rcsaquino <rcsaquino.md@gmail.com>
2025-05-29 21:01:22 +0800

    Added zen user.js

---
+++ other_configs/user.js
@@ -0,0 +1,26 @@
+// General - Startup
+user_pref("browser.startup.page", 1);
+user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
+
+// Look and Feel - Sidebar and tabs layout
+user_pref("zen.view.use-single-toolbar", false);
+user_pref("zen.view.show-bottom-border", true);
+user_pref("zen.view.show-newtab-button-top", false);
+
+// Tab Management - Tab Unloader
+user_pref("zen.tab-unloader.enabled", false);
+
+// Tab Management - Pinned Tabs
+user_pref(
+  "zen.pinned-tab-manager.close-shortcut-behavior",
+  "reset-unload-switch"
+);
+user_pref("zen.pinned-tab-manager.restore-pinned-tabs-to-pinned-url", true);
+
+// Search
+user_pref("browser.search.separatePrivateDefault", false);
+user_pref("browser.search.suggest.enabled", true);
+
+// Others
+user_pref("full-screen-api.transition-duration.enter", "150 150");
+user_pref("full-screen-api.transition-duration.leave", "150 150");
