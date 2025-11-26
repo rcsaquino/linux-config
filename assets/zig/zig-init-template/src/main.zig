d09b50a3e8be0253d26af13fefe2b4cc74ce4e0b
rcsaquino <rcsaquino.md@gmail.com>
2025-11-27 00:25:48 +0800

    Refactor template
    Rename custom_allocator.zig to utils.zig Update build.zig to pass
    arguments to run command.

--- assets/zig/zig-init-template/src/main.zig
+++ assets/zig/zig-init-template/src/main.zig
@@ -1,9 +1,5 @@
 const std = @import("std");
-const ca = @import("custom_allocator.zig").init(.Adaptive);
 
 pub fn main() void {
-    const alloc = ca.allocator();
-    defer ca.deinit();
-
-    _ = alloc;
+    std.debug.print("Hello, world!\n", .{});
 }
